dd187cbbfbf85a749a15275ab2f9496dbd818286
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-10-15 19:06:17 -0400

    design in customer details, adeed filters years

--- src/components/customers/CustomerDetails.vue
+++ src/components/customers/CustomerDetails.vue
@@ -1,115 +1,111 @@
 <template>
-  <div class="fill-height overflow-x-hidden">
-    <v-row dense>
-      <v-col cols="3">
-        <v-list dense rounded>
-          <v-subheader>Meses</v-subheader>
-          <v-list-item link v-for="(item, i) in items" :key="i">
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-col>
-      <v-col cols="9">
-        <v-card class="pa-5" flat>
-          <div>
-            <small>Empresa</small>
-          </div>
-          <div class="title font-weight-bold">
-            <span v-text="customer"></span>
-          </div>
-          <v-row dense>
-            <v-col cols="12">
+  <v-row dense>
+    <v-col cols="12">
+      <v-card flat>
+        <div class="title font-weight-bold">
+          <v-row align="center">
+            <v-col cols="4">
               <div>
-                Arquivos Janeiro 2020
+                <small>Empresa</small>
+                <div>
+                  <span v-text="$route.params.cnpj"></span>
+                </div>
               </div>
-              <v-divider></v-divider>
             </v-col>
+            <div>
+              <v-row>
+                <v-col v-for="(item, i) in getYears" :key="i">
+                  <v-chip
+                    @click="filterYear(item)"
+                    :color="selected(item)"
+                    link
+                    :text-color="parseInt(item) === year ? 'white' : ''"
+                  >
+                    Ano {{ item }}</v-chip
+                  >
+                </v-col>
+              </v-row>
+            </div>
           </v-row>
-          <v-row dense>
-            <v-col cols="12">
-              <v-card :loading="isLoading">
-                <v-list-item>
-                  <v-list-item-avatar tile>
-                    <v-img src="@/assets/icons/download-folder.png"></v-img>
-                  </v-list-item-avatar>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      <span>Terceiros</span>
-                    </v-list-item-title>
-                    <v-list-item-subtitle>
-                      <span>154 MB</span>
-                    </v-list-item-subtitle>
-                  </v-list-item-content>
-                  <v-list-item-action>
-                    <v-list-item-icon>
-                      <v-btn flat icon color="primary">
-                        <v-icon>mdi-download-box</v-icon>
-                      </v-btn>
-                    </v-list-item-icon>
-                  </v-list-item-action>
-                </v-list-item>
-              </v-card>
-            </v-col>
-            <v-col cols="12">
-              <v-card :loading="isLoading">
-                <v-list-item>
-                  <v-list-item-avatar tile>
-                    <v-img src="@/assets/icons/download-folder.png"></v-img>
-                  </v-list-item-avatar>
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      <span>Terceiros</span>
-                    </v-list-item-title>
-                    <v-list-item-subtitle>
-                      <span>154 MB</span>
-                    </v-list-item-subtitle>
-                  </v-list-item-content>
-                  <v-list-item-action>
-                    <v-list-item-icon>
-                      <v-btn flat icon color="primary">
-                        <v-icon>mdi-download-box</v-icon>
-                      </v-btn>
-                    </v-list-item-icon>
-                  </v-list-item-action>
-                </v-list-item>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-    </v-row>
-  </div>
+        </div>
+        <v-divider></v-divider>
+        <v-row v-if="Customer.children">
+          <v-col
+            cols="12"
+            sm="2"
+            v-for="(item, i) in Customer.children.filter(
+              (item) => item.path.indexOf('.zip') < 0
+            )"
+            :key="i"
+          >
+            <CardFile @download="getFiles" :month="item" :year="year" />
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
-
 <script>
+import CardFile from "@/components/customers/CardFile.vue";
 export default {
+  components: {
+    CardFile,
+  },
   mounted() {
-    this.customer = this.$route.params.cnpj;
+    this.getFiles();
   },
   data() {
     return {
       isLoading: true,
-      customer: null,
       item: 1,
-      items: [
-        { text: "Janeiro", icon: "mdi-calendar-month" },
-        { text: "Fevereiro", icon: "mdi-calendar-month" },
-        { text: "Março", icon: "mdi-calendar-month" },
-        { text: "Abril", icon: "mdi-calendar-month" },
-        { text: "Maio", icon: "mdi-calendar-month" },
-        { text: "Junho", icon: "mdi-calendar-month" },
-        { text: "Julho", icon: "mdi-calendar-month" },
-        { text: "Agosto", icon: "mdi-calendar-month" },
-        { text: "Setembro", icon: "mdi-calendar-month" },
-        { text: "Novembro", icon: "mdi-calendar-month" },
-        { text: "Dezembro", icon: "mdi-calendar-month" },
-      ],
+      year: new Date().getFullYear(),
     };
+  },
+  computed: {
+    Customer() {
+      let customer;
+
+      if (this.$store.state.customers.customerSelected) {
+        this.$store.state.customers.customerSelected.children.filter((item) => {
+          if (parseInt(item.name) === parseInt(this.year)) {
+            customer = item;
+          }
+        });
+        localStorage.setItem("sizes", JSON.stringify(customer));
+        return customer;
+      }
+      return {};
+    },
+    getYears() {
+      return this.$store.getters["customers/getYears"] || [];
+    },
+  },
+  methods: {
+    async getFiles() {
+      if (this.$route.params.cnpj) {
+        await this.$store.dispatch("customers/request", {
+          state: "customerSelected",
+          method: "post",
+          url: "/folder-param",
+          data: {
+            getPath: JSON.stringify({
+              value: localStorage.getItem("customer"),
+            }),
+            depth: 3,
+          },
+        });
+      }
+    },
+    selected(item) {
+      if (item == this.year) {
+        return "light-blue accent-4";
+      }
+      return "grey lighten-5";
+    },
+    filterYear(item) {
+      this.year = parseInt(item);
+      console.log(this.year);
+    },
   },
 };
 </script>
