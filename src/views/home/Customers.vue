5c7181308f5b2b3970ba330a34331da9c04cbdb2
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-10-15 19:08:35 -0400

    customer view page

--- src/views/home/Customers.vue
+++ src/views/home/Customers.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="px-3">
-    <v-row dense class="my-5">
-      <v-col cols="12">
-        <router-view></router-view>
-      </v-col>
-    </v-row>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
