6c2ff046d2b1c142355d36823b75c9b54a2c207b
jhonatanborg <jhonatanborgesdj@gmail.com>
2020-10-15 19:07:07 -0400

    new menu logo create

--- src/components/shared/Menu.vue
+++ src/components/shared/Menu.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app-bar app color="light-blue accent-4" flat dark>
     <v-container class="py-0 fill-height">
-      <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+      <v-avatar class="mr-10" color="grey darken-1" size="32"
+        ><v-img src="@/assets/logo.png"></v-img
+      ></v-avatar>
       <v-btn v-for="link in links" :key="link" text>
         {{ link }}
       </v-btn>
